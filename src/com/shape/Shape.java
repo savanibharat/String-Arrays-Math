656a12a6fc8d3bfc303ba5eca95bd21744b30919
BHARAT SAVANI <savanibharat@gmail.com>
2014-11-29 22:45:22 -0800

    Shape Interface

--- src/com/shape/Shape.java
+++ src/com/shape/Shape.java
@@ -3,5 +3,11 @@ package com.shape;
 public interface Shape {
 
 	//Insert methods
+	Point[] getCoOrdinates();
+	double perimeter();
+	double area();
+	double diagonal();
+	boolean contains(Point p);
+	String getShape();
 	
 }
