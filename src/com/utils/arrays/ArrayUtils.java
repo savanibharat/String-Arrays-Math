290a8cf5dcc985bc710658786f75b6032c32ec86
BHARAT SAVANI <savanibharat@gmail.com>
2014-12-10 01:32:26 -0800

    is Collection empty

--- src/com/utils/arrays/ArrayUtils.java
+++ src/com/utils/arrays/ArrayUtils.java
@@ -1,5 +1,23 @@
 package com.utils.arrays;
 
+import java.util.Collection;
+
 public class ArrayUtils {
 
+	/**
+	 * Checks if is empty.
+	 *
+	 * @param <E>
+	 *            the element type
+	 * @param collection
+	 *            the collection
+	 * @return true, if is empty
+	 */
+	public static <E> boolean isEmpty(Collection<? super E> collection) {
+
+		if ((collection.size() == 0) || (collection == null))
+			return true;
+		return false;
+	}
+	
 }
