c3d73d3332d89bcb0b3e89f29724567093a7a825
BHARAT SAVANI <savanibharat@gmail.com>
2014-12-11 13:11:17 -0800

    Union of 2 Sets

--- src/com/utils/arrays/ArrayUtils.java
+++ src/com/utils/arrays/ArrayUtils.java
@@ -1,6 +1,10 @@
 package com.utils.arrays;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class ArrayUtils {
 
@@ -20,4 +24,48 @@ public class ArrayUtils {
 		return false;
 	}
 	
+	/**
+	 * Union.
+	 *
+	 * @param <T>
+	 *            the generic type
+	 * @param setA
+	 *            the set a
+	 * @param setB
+	 *            the set b
+	 * @return the sets the
+	 */
+	public static <T> Set<T> union(Set<T> setA, Set<T> setB) {
+
+		// SetA empty & SetB has values
+		if (isEmpty(setA) && !isEmpty(setB)) {
+			return Collections.unmodifiableSet(setB);
+		}
+
+		// SetA has values & SetB is empty
+		if (!isEmpty(setA) && isEmpty(setB)) {
+			return Collections.unmodifiableSet(setA);
+		}
+
+		// Both set are empty
+		if (isEmpty(setA) && isEmpty(setB)) {
+			return new LinkedHashSet<T>();
+		}
+
+		if (setA.equals(setB)) {
+			return setA;
+		}
+
+		Set<T> setC = new LinkedHashSet<T>();
+		Iterator<T> iterA = setA.iterator();
+		Iterator<T> iterB = setB.iterator();
+
+		while (iterA.hasNext()) {
+			setC.add(iterA.next());
+		}
+		while (iterB.hasNext()) {
+			setC.add(iterB.next());
+		}
+		return Collections.unmodifiableSet(setC);
+	}
 }
