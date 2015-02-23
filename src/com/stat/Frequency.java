c28c1d00497b38a0cf226f0f53de9d7c85671e3e
BHARAT SAVANI <savanibharat@gmail.com>
2015-02-23 15:13:46 -0800

    Frequency class functionality added

--- src/com/stat/Frequency.java
+++ src/com/stat/Frequency.java
@@ -6,6 +6,7 @@ public class Frequency {
 
 	TreeMap<String, Long> freq;
 	private static final long INCREMENT_VALUE = 1;
+	private static final long ZERO_VALUE=0;
 
 	public Frequency() {
 		freq = new TreeMap<String, Long>();
@@ -16,23 +17,41 @@ public class Frequency {
 	}
 	
 	public void add(char value) {
-		addInTable(String.valueOf(value));
+		addInTable(convert(value));
 	}
-
+	
+	private String convert(char value){
+		return String.valueOf(value);
+	}
+	
 	public void add(double value) {
-		addInTable(String.valueOf(value));
+		addInTable(convert(value));
+	}
+	
+	private String convert(double value){
+		return String.valueOf(value);
 	}
 	
 	public void add(int value) {
-		addInTable(String.valueOf(new Double(value)));
+		addInTable(convert(value));
+	}
+	
+	private String convert(int value){
+		return String.valueOf(new Double(value));
 	}
 
 	public void add(float value) {
-		addInTable(String.valueOf(new Double(value)));
+		addInTable(convert(value));
+	}
+	
+	private String convert(float value){
+		return String.valueOf(new Double(value));
 	}
 
+	public void add(String value){
+		addInTable(value);
+	}
 	
-
 	public void addInTable(String value) {
 
 		/**
@@ -46,7 +65,46 @@ public class Frequency {
 			freq.put(value, INCREMENT_VALUE);
 		}
 	}
+	
+	public Long getCount(char value){
+		return getCount(convert(value));/*
+		if(freq.size()>0){
+			return freq.get(convert(value));
+		}
+		else{
+			return ZERO_VALUE;
+		}*/
+	}
+	
+	public Long getCount(int value){
+		return getCount(convert(value));
+	}
 
+	public Long getCount(float value){
+		return getCount(convert(value));
+	}
+	
+	public Long getCount(double value){
+		return getCount(convert(value));
+	}
+	
+	public Long getCount(String value){
+		if(!isEmpty()){
+			if(freq.containsKey(value)){
+				return freq.get(value);
+			}
+			else{
+				return ZERO_VALUE;
+			}
+		}else{
+			return ZERO_VALUE;
+		}
+	}
+	
+	public boolean isEmpty(){
+		return freq.isEmpty();
+	}
+	
 	public String toString() {
 		return freq.toString();
 	}
@@ -56,10 +114,10 @@ public class Frequency {
 		f.add(1);
 		f.add('a');
 		f.add('a');
-		f.add('a');
+		f.add("aaa");
 		f.add(1.0f);
 		f.add(1.0);
-		System.out.println(new Float(new Integer(1)));
+		System.out.println(f.getCount('z'));
 		System.out.println(f.toString());
 	}
 
