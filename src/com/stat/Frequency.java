be14033a9f8c7343ac4a0259373d601d204ed4bd
BHARAT SAVANI <savanibharat@gmail.com>
2015-02-24 18:35:19 -0800

    Frequency class completed

--- src/com/stat/Frequency.java
+++ src/com/stat/Frequency.java
@@ -1,12 +1,14 @@
 package com.stat;
 
+import java.util.Iterator;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 
 public class Frequency {
 
 	TreeMap<String, Long> freq;
 	private static final long INCREMENT_VALUE = 1;
-	private static final long ZERO_VALUE=0;
+	private static final long ZERO_VALUE = 0;
 
 	public Frequency() {
 		freq = new TreeMap<String, Long>();
@@ -15,48 +17,53 @@ public class Frequency {
 	public void clear() {
 		freq.clear();
 	}
-	
+
 	public void add(char value) {
 		addInTable(convert(value));
 	}
-	
-	private String convert(char value){
+
+	private String convert(char value) {
 		return String.valueOf(value);
 	}
-	
+
+	/*
+	 * public long getPercent(char value){
+	 * 
+	 * }
+	 */
+
 	public void add(double value) {
 		addInTable(convert(value));
 	}
-	
-	private String convert(double value){
+
+	private String convert(double value) {
 		return String.valueOf(value);
 	}
-	
+
 	public void add(int value) {
 		addInTable(convert(value));
 	}
-	
-	private String convert(int value){
+
+	private String convert(int value) {
 		return String.valueOf(new Double(value));
 	}
 
 	public void add(float value) {
 		addInTable(convert(value));
 	}
-	
-	private String convert(float value){
+
+	private String convert(float value) {
 		return String.valueOf(new Double(value));
 	}
 
-	public void add(String value){
+	public void add(String value) {
 		addInTable(value);
 	}
-	
+
 	public void addInTable(String value) {
 
 		/**
-		 * K is String 
-		 * V is count of K
+		 * K is String V is count of K
 		 * */
 		if (freq.containsKey(value)) {
 			long previousCount = freq.get(value);
@@ -65,46 +72,101 @@ public class Frequency {
 			freq.put(value, INCREMENT_VALUE);
 		}
 	}
-	
-	public Long getCount(char value){
-		return getCount(convert(value));/*
-		if(freq.size()>0){
-			return freq.get(convert(value));
-		}
-		else{
-			return ZERO_VALUE;
-		}*/
-	}
-	
-	public Long getCount(int value){
+
+	public long getCount(char value) {
 		return getCount(convert(value));
 	}
 
-	public Long getCount(float value){
+	public long getCount(int value) {
 		return getCount(convert(value));
 	}
-	
-	public Long getCount(double value){
+
+	public long getCount(float value) {
 		return getCount(convert(value));
 	}
-	
-	public Long getCount(String value){
-		if(!isEmpty()){
-			if(freq.containsKey(value)){
-				return freq.get(value);
-			}
-			else{
+
+	public long getCount(double value) {
+		return getCount(convert(value));
+	}
+
+	public long getCount(String value) {
+		if (!isEmpty()) {
+			if (freq.containsKey(value)) {
+				return freq.get(value).longValue();
+			} else {
 				return ZERO_VALUE;
 			}
-		}else{
+		} else {
 			return ZERO_VALUE;
 		}
 	}
-	
-	public boolean isEmpty(){
+
+	public int getKeysCount() {
+		return freq.keySet().size();
+	}
+
+	public long getValuesCount() {
+		Iterator<Long> iter = freq.values().iterator();
+		Long values = 0L;
+		while (iter.hasNext()) {
+			values = values + iter.next();
+		}
+		return values.longValue();
+	}
+
+	public double getKeyPercent(char value) {
+		return getKeyPercent(convert(value));
+	}
+
+	public double getKeyPercent(int value) {
+		return getKeyPercent(convert(value));
+	}
+
+	public double getKeyPercent(float value) {
+		return getKeyPercent(convert(value));
+	}
+
+	public double getKeyPercent(double value) {
+		return getKeyPercent(convert(value));
+	}
+
+	public float getKeyPercent(String value) {
+		if (!isEmpty()) {
+			if (freq.containsKey(value)) {
+				return ((float) freq.get(value) / getValuesCount());
+			} else {
+				return ZERO_VALUE;
+			}
+		} else {
+			return ZERO_VALUE;
+		}
+	}
+
+	public Iterator<Entry<String, Long>> frequencyIterator() {
+		return freq.entrySet().iterator();
+	}
+
+	public void insertFrequencies(Frequency frequency) {
+
+		Iterator<Entry<String, Long>> iter = frequency.frequencyIterator();
+		while (iter.hasNext()) {
+			Entry<String, Long> entry = iter.next();
+			System.out.println(entry+".");
+			String K = entry.getKey();
+			Long V = entry.getValue();
+			if (freq.containsKey(K)) {
+				freq.put(K, freq.get(K) + V);
+			} else {
+				freq.put(K, V);
+			}
+		}
+		System.out.println(freq);
+	}
+
+	public boolean isEmpty() {
 		return freq.isEmpty();
 	}
-	
+
 	public String toString() {
 		return freq.toString();
 	}
@@ -115,10 +177,19 @@ public class Frequency {
 		f.add('a');
 		f.add('a');
 		f.add("aaa");
+		f.add("aaa");
+		f.add("aaa");
 		f.add(1.0f);
 		f.add(1.0);
-		System.out.println(f.getCount('z'));
-		System.out.println(f.toString());
+		Frequency f1 = new Frequency();
+		f1.add(1);
+		f1.add('a');
+		f1.add("aaa");
+		f1.add("aaa");
+		f1.add("aaa");
+		f1.add(1.0f);
+		f1.add(1.0);
+		f1.insertFrequencies(f);
 	}
 
 }
